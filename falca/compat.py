95fb548e0eec71bd01cc3800f82f9d8ddb62f15b
aprilahijriyan <hijriyan23@gmail.com>
2021-05-10 21:52:43 +0700

    🐛 FIX: marshmallow keys (int -> str)

--- falca/compat.py
+++ falca/compat.py
@@ -8,6 +8,10 @@ try:
     _dumps = json.dumps
 
     def dumps(*args, **kwds):
+        option = kwds.get("option")
+        if not option:
+            option = json.OPT_NON_STR_KEYS
+        kwds["option"] = option
         return _dumps(*args, **kwds).decode()
 
     json.dumps = dumps
