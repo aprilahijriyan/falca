fd50cd25d24297a231156b296e6178ad5023c583
aprilahijriyan <hijriyan23@gmail.com>
2021-05-11 08:16:29 +0700

    🚧 Work in Progress:  Asynchronous middleware

--- falca/middleware/base.py
+++ falca/middleware/base.py
@@ -15,6 +15,45 @@ class Middleware(object):
         self.app = app
         super().__init__()
 
+    async def process_startup(self, scope, event):
+        """Process the ASGI lifespan startup event.
+
+        Invoked when the server is ready to start up and
+        receive connections, but before it has started to
+        do so.
+
+        To halt startup processing and signal to the server that it
+        should terminate, simply raise an exception and the
+        framework will convert it to a "lifespan.startup.failed"
+        event for the server.
+
+        Args:
+            scope (dict): The ASGI scope dictionary for the
+                lifespan protocol. The lifespan scope exists
+                for the duration of the event loop.
+            event (dict): The ASGI event dictionary for the
+                startup event.
+        """
+
+    async def process_shutdown(self, scope, event):
+        """Process the ASGI lifespan shutdown event.
+
+        Invoked when the server has stopped accepting
+        connections and closed all active connections.
+
+        To halt shutdown processing and signal to the server
+        that it should immediately terminate, simply raise an
+        exception and the framework will convert it to a
+        "lifespan.shutdown.failed" event for the server.
+
+        Args:
+            scope (dict): The ASGI scope dictionary for the
+                lifespan protocol. The lifespan scope exists
+                for the duration of the event loop.
+            event (dict): The ASGI event dictionary for the
+                shutdown event.
+        """
+
     def process_request(self, req: Request, resp: Response):
         """Process the request before routing it.
 
@@ -48,6 +87,19 @@ class Middleware(object):
                 routed. May be None if no route was found
                 for the request.
         """
+
+    async def process_request_async(self, req: Request, resp: Response):
+        pass
+
+    async def process_resource_async(
+        self, req: Request, resp: Response, resource: object, *args
+    ):
+        pass
+
+    async def process_response_async(
+        self, req: Request, resp: Response, resource: object, *args
+    ):
+        pass
 
     def is_valid_content_type(self, req: Request):
         if (
