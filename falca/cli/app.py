b6caae3c00727b87b5de6fcf5384353a8c0a838c
aprilahijriyan <hijriyan23@gmail.com>
2021-05-12 17:47:48 +0700

    🚧 Work in Progress:  CLI Support

---
+++ falca/cli/app.py
@@ -0,0 +1,88 @@
+import os
+from typing import Any, Callable, Dict, Optional, Type
+
+import click
+from pkg_resources import get_distribution
+from rich import print as cprint
+from typer import Context, Exit, Option, Typer
+from typer.models import Default
+
+from ..helpers import import_attr
+from .inspect import inspect_command
+
+
+class Command(Typer):
+    def __init__(
+        self,
+        *,
+        name: Optional[str] = Default(None),
+        cls: Optional[Type[click.Command]] = Default(None),
+        # invoke_without_command: bool = Default(False),
+        no_args_is_help: Optional[bool] = Default(None),
+        subcommand_metavar: Optional[str] = Default(None),
+        chain: bool = Default(False),
+        result_callback: Optional[Callable[..., Any]] = Default(None),
+        # Command
+        context_settings: Optional[Dict[Any, Any]] = Default(None),
+        # callback: Optional[Callable[..., Any]] = Default(None),
+        help: Optional[str] = Default(None),
+        epilog: Optional[str] = Default(None),
+        short_help: Optional[str] = Default(None),
+        options_metavar: str = Default("[OPTIONS]"),
+        add_help_option: bool = Default(True),
+        hidden: bool = Default(False),
+        deprecated: bool = Default(False),
+        add_completion: bool = True,
+    ):
+        invoke_without_command = True
+        callback = self.init
+        super().__init__(
+            name=name,
+            cls=cls,
+            invoke_without_command=invoke_without_command,
+            no_args_is_help=no_args_is_help,
+            subcommand_metavar=subcommand_metavar,
+            chain=chain,
+            result_callback=result_callback,
+            context_settings=context_settings,
+            callback=callback,
+            help=help,
+            epilog=epilog,
+            short_help=short_help,
+            options_metavar=options_metavar,
+            add_help_option=add_help_option,
+            hidden=hidden,
+            deprecated=deprecated,
+            add_completion=add_completion,
+        )
+
+    def init(
+        self,
+        ctx: Context,
+        version: Optional[bool] = Option(
+            None, "--version", is_eager=True, help="Show version number and exit"
+        ),
+    ):
+        if ctx.resilient_parsing:
+            return
+
+        if version:
+            version = get_distribution("falca").version
+            cprint(f":package: Falca v{version}")
+            raise Exit
+
+        ctx.app = load_app()
+
+
+cli = Command(name="falca", help="Falca Command")
+cli.command("inspect")(inspect_command)
+
+
+def load_app():
+    try:
+        src = os.environ.get("FALCA_APP", "app.app")
+        app = import_attr(src)
+        return app
+    except ImportError:
+        cprint("[red]Error:[/red] can't find app :eyes:")
+        raise Exit
---
+++ falca/cli/inspect.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+from rich import print as cprint
+from rich.tree import Tree
+from typer import Context
+
+from ..app import ASGI, WSGI
+
+
+def parse_tree(tree: Tree, routes: list):
+    pass
+
+
+def get_routes(app: Union[WSGI, ASGI]):
+    roots = app._router._roots
+    routers = app.routers
+    root = Tree(":bookmark_tabs: Resources", highlight=True)
+    parse_tree(root, roots)
+    for router in routers:
+        roots = router._roots
+        child = root.add(f":open_file_folder: {router.url_prefix}")
+        parse_tree(child, roots)
+
+
+def inspect_command(ctx: Context):
+    """
+    Inspect routes
+    """
+
+    tree = get_routes(ctx.app)
+    cprint(tree)
