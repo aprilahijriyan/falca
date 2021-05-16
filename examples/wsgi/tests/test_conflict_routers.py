078c5dacffa0ff047eeb30ea683fa79fe6bd4da2
aprilahijriyan <hijriyan23@gmail.com>
2021-05-17 01:13:48 +0700

    ⚡ Test: Added endpoint conflict tests

---
+++ examples/wsgi/tests/test_conflict_routers.py
@@ -0,0 +1,32 @@
+from app import app
+from pytest import raises
+
+from falca.exceptions import BadRouter, EndpointConflict
+from falca.resource import Resource
+from falca.router import Router
+
+
+class Simple(Resource):
+    def on_get(self):
+        pass
+
+
+def test_bad_router():
+    router = Router()
+    router.add_route("/api", Simple())
+    with raises(BadRouter):
+        app.include_router(router)
+
+
+def test_conflict_endpoint():
+    app.add_route("/simple", Simple())
+    with raises(EndpointConflict):
+        app.add_route("/simple", Simple())
+
+
+def test_conflict_router():
+    router = Router(url_prefix="/simple")
+    router.add_route("/api", Simple())
+    app.include_router(router)
+    with raises(EndpointConflict):
+        app.include_router(router)
