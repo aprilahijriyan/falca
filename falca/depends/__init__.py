09d93c35b98f7d58234e4d612e66ec5d7675a6e9
aprilahijriyan <hijriyan23@gmail.com>
2021-05-24 01:23:11 +0700

    ⚡ Update:  Add dependency 'Settings'

--- falca/depends/__init__.py
+++ falca/depends/__init__.py
@@ -8,6 +8,18 @@ class Plugin(Depends):
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def __call__(self, *args, request: Union[Request, ASGIRequest] = None):
+    def __call__(self, *args, request: Union[Request, ASGIRequest]):
         app = request.context.app
         return app.plugins.get(self.name)
+
+
+class Settings(Depends):
+    def __init__(self, name: str = None) -> None:
+        self.name = name
+
+    def __call__(self, *args, request: Union[Request, ASGIRequest]):
+        app = request.context.app
+        settings = app.settings
+        if self.name is not None:
+            settings = settings.get(self.name)
+        return settings
